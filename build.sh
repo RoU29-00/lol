4d73c83adac2002a9ce08b11fee6971b3d69956e
Ando Junichiro <83817509+RoU29-00@users.noreply.github.com>
2025-08-03 20:22:07 +0900

    settings

---
+++ build.sh
@@ -0,0 +1,8 @@
+#!/user/binenv bash
+#exist on erorr
+set -o errexit
+
+pip install -r requirements.txt
+python manage.py collectstatic --no-input
+python manage.py migrate
+python manage.py superuser
